93d15ae9a19564291ac293f453f2ddd82838053b
deepak-ranolia <d.ranolia92@gmail.com>
2019-07-29 12:42:24 +0530

    update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,7 +10,7 @@ import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor( private messageService: MessageService ) { }
 
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
